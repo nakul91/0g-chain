cc4f72b16596c41801963d7048064061bcbe3708
Solovyov1796 <xinyu412@live.com>
2024-05-01 12:26:39 +0800

    revise file structure in cmd

--- cmd/0gchaind/root.go
+++ cmd/0gchaind/root.go
@@ -1,4 +1,4 @@
-package cmd
+package main
 
 import (
 	"fmt"
@@ -25,7 +25,7 @@ import (
 
 	"github.com/0glabs/0g-chain/app"
 	"github.com/0glabs/0g-chain/app/params"
-	"github.com/0glabs/0g-chain/cmd/kava/opendb"
+	"github.com/0glabs/0g-chain/cmd/opendb"
 )
 
 // EnvPrefix is the prefix environment variables must have to configure the app.
